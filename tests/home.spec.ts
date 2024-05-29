33a270e15db04a99b79bc9fe77a605c94ede6ede
Jernito <jernito.naisso@gmail.com>
2024-05-29 11:43:21 +0200

    ceci est un test

--- tests/home.spec.ts
+++ tests/home.spec.ts
@@ -4,5 +4,8 @@ test('has title', async ({ page }) => {
     await page.goto('https://demowebshop.tricentis.com/');
   
     // Expect a title "to contain" a substring.
+
+    //ceci est un test
+    
     await expect(page).toHaveTitle('Demo Web Shop');
   });
